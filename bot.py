41d407f99dbbaeb61bc97483ab757b86aef8ca5e
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-03-17 17:47:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 from pyrogram import Client
+
+
 PyrogramBot = Client(
+    "ADV GUARDIAN GROOT"
     api_hash="5023c40ea655bc2834e48888b17ccee8", 
     api_id="13160306", 
     bot_token="5221793107:AAGzaFdLJoZIBmKB1MeOLJCJRfK0rFfYkVg", 
