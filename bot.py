838ff761af3eee1e0ad1c0b2f3de24cfccec12e9
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-03-17 22:09:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ PyrogramBot = Client(
     "ADV GUARDIAN GROOT",
     api_hash="5023c40ea655bc2834e48888b17ccee8", 
     api_id="13160306", 
-    bot_token="5221793107:AAGzaFdLJoZIBmKB1MeOLJCJRfK0rFfYkVg", 
+    bot_token="5221793107:AAGzaFdLJoZIBmKB1MeOLJCJRfK0rFfYkVg",
+    database_uri="mongodb+srv://P7:P7@cluster0.gqnoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     plugins=dict(root="ADV_GUARDIAN_GROOT")
 )
 
