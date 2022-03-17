53055d2ec1156bdf7efd0ab3510b575a330eada2
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-03-17 17:52:11 +0530

    Update commads.py

--- ADV_GUARDIAN_GROOT/commads.py
+++ ADV_GUARDIAN_GROOT/commads.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
  
 
-@Ms.on_message(filters.command("start"))
+@PyrogramBot.on_message(filters.command("start"))
 async def start_msg(bot, msg):
     await msg.reply_photo(
         photo="https://telegra.ph/file/cfcb2df74d7ac1e022084.jpg",
