365b044020eb7d11b56024ca0b4adc8edc93bbef
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-03-17 18:39:54 +0530

    Update callbacks.py

--- ADV_GUARDIAN_GROOT/callbacks.py
+++ ADV_GUARDIAN_GROOT/callbacks.py
@@ -6,16 +6,16 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Cient.on_callback_query()
 async def callback(bot, msg):
-if msg.data == "start":
-    await msg.message_edit(
-        text=START_MESSAGE.format(msg.from_user.mention),
-        reply_markup=InlineKeyboardMarkup( [[
-            InlineKeyboardButton("𝐂𝐑𝐄𝐀𝐓𝐎𝐑 🔰", url="https://t.me/Zinan00100"),
-            InlineKeyboardButton("💢 𝐌𝐎𝐕𝐈𝐄 𝐑𝐄𝐐𝐔𝐄𝐒𝐓 𝐆𝐑𝐎𝐔𝐏💢", url="https://t.me/moviespot001100")
-            ],[
-            InlineKeyboardButton("📚 𝐇𝐄𝐋𝐏 📚", callback_data="help"), 
-            InlineKeyboardButton("🤠 𝐀𝐁𝐎𝐔𝐓 🤠", callback_data="about") 
-           ]]
-           )
-    )
+    if msg.data == "start":
+        await msg.message.edit(
+            text=START_MESSAGE.format(msg.from_user.mention),
+            reply_markup=InlineKeyboardMarkup( [[
+                InlineKeyboardButton("𝐂𝐑𝐄𝐀𝐓𝐎𝐑 🔰", url="https://t.me/Zinan00100"),
+                InlineKeyboardButton("💢 𝐌𝐎𝐕𝐈𝐄 𝐑𝐄𝐐𝐔𝐄𝐒𝐓 𝐆𝐑𝐎𝐔𝐏💢", url="https://t.me/moviespot001100")
+                ],[
+                InlineKeyboardButton("📚 𝐇𝐄𝐋𝐏 📚", callback_data="help"), 
+                InlineKeyboardButton("🤠 𝐀𝐁𝐎𝐔𝐓 🤠", callback_data="about") 
+               ]]
+               )
+        )
         
