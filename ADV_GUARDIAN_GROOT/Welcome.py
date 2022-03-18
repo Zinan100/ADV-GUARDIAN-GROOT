8de7e5add0b70e42152f893ba9ceac59dc8a9ec8
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-03-18 20:48:38 +0530

    Update Welcome.py

--- ADV_GUARDIAN_GROOT/Welcome.py
+++ ADV_GUARDIAN_GROOT/Welcome.py
@@ -14,7 +14,7 @@ import logging
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
  chatid= message.chat.id
- await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
+ await bot.send_message(text=f"Welcome {message.from_user.mention} to {title} ,  Happy to have here",chat_id=chatid)
  
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
