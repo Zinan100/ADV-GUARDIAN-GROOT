03a705c5d198c1b8ebd380c56c97f7186270aa5a
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-03-19 15:19:03 +0530

    Update commads.py

--- ADV_GUARDIAN_GROOT/commads.py
+++ ADV_GUARDIAN_GROOT/commads.py
@@ -12,7 +12,8 @@ async def start_msg(bot, msg):
         photo="https://telegra.ph/file/cfcb2df74d7ac1e022084.jpg",
         caption=START_MESSAGE.format(msg.from_user.mention),
         reply_markup=InlineKeyboardMarkup( [[
-            InlineKeyboardButton("🔰 𝐂𝐑𝐄𝐀𝐓𝐎𝐑 🔰", url="https://t.me/Zinan00100"),
+            InlineKeyboardButton("🔰 𝐂𝐑𝐄𝐀𝐓𝐎𝐑 🔰", url="https://t.me/Zinan00100") 
+            ],[
             InlineKeyboardButton("💢 𝐌𝐎𝐕𝐈𝐄 𝐑𝐄𝐐𝐔𝐄𝐒𝐓 𝐆𝐑𝐎𝐔𝐏💢", url="https://t.me/moviespot001100")
             ],[
             InlineKeyboardButton("📚 𝐇𝐄𝐋𝐏 📚", callback_data="help"), 
