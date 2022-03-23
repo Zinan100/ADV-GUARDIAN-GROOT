c9f5c29921cce396de3122d56fb1d4ef3529bf67
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-03-23 20:07:32 +0530

    Update callbacks.py

--- ADV_GUARDIAN_GROOT/callbacks.py
+++ ADV_GUARDIAN_GROOT/callbacks.py
@@ -214,7 +214,7 @@ To Get Sticker ID
         await msg.message.edit(
             text=random.choice(All_Message)
         )
-        await asyncio.sleep(2)
+        await asyncio.sleep(4)
         await msg.message.edit(
             text="ᴛyᴩᴇ /start ᴛᴏ ʙᴇɢɪɴ ᴀɢᴀɪɴ"
         )
