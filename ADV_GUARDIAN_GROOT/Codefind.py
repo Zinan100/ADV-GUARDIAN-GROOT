8bc1e3fa20511072e2bf90d564833d41d05ef2bc
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-03-23 09:59:04 +0530

    Update Codefind.py

--- ADV_GUARDIAN_GROOT/Codefind.py
+++ ADV_GUARDIAN_GROOT/Codefind.py
@@ -4,14 +4,14 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("ans"))
 async def fil_mod(bot, msg):
-      mode_on = ["18gffvj", "182cdgbbr2", "18ygfvtyv12"]
+      mode_on = ["18gffvj", "182cd2", "18yg12"]
       mode_of = ["vvgg", "shvdd", "ggffgy"]
 
       try:
          args = msg.text.split(None, 1)[1].lower()
       except:
-         return await msg.reply("checking answer")
-      m = await msg.reply_text("checking answer in progress")
+         return await msg.reply("ɪɴɢʟᴜᴅᴇ yᴏᴜʀᴇ ᴀɴꜱᴡᴇʀ ᴡɪᴛʜ ᴄᴏᴍᴍᴀɴᴅ ᴇxᴀᴍᴩʟᴇ:/ans 12g ᴛʜᴇ ᴀɴꜱᴡᴇʀ ᴏɴʟy ʜᴀꜱ 6 ᴄʜᴀʀᴇᴄᴛᴇʀꜱ")
+      m = await msg.reply_text("ɪɴɢʟᴜᴅᴇ yᴏᴜʀᴇ ᴀɴꜱᴡᴇʀ ᴡɪᴛʜ ᴄᴏᴍᴍᴀɴᴅ ᴇxᴀᴍᴩʟᴇ:/ans 12g ᴛʜᴇ ᴀɴꜱᴡᴇʀ ᴏɴʟy ʜᴀᴠᴇ 6 ᴄʜᴀʀᴇᴄᴛᴇʀꜱ")
 
       if args in mode_on:
           await m.edit("✅️You are correct")
