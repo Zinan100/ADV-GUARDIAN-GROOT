bab63da2666eb3fff4fe2a94409199a760544c2e
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-03-30 22:14:31 +0530

    Update luck.py

--- ADV_GUARDIAN_GROOT/Fun/luck.py
+++ ADV_GUARDIAN_GROOT/Fun/luck.py
@@ -3,8 +3,6 @@
 
 
 from pyrogram import Client, filters
-from info import COMMAND_HAND_LER
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
 # LUCK------------ https://telegram.me/Josprojects ------------ #
 
