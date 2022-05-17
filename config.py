1fa28a4be1cd31be299dd4276f89110cd36c05b8
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-05-17 17:26:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ id_pattern = re.compile(r'^.\d+$')
 API_HASH = os.environ.get("API_HASH")
 API_ID = int(os.environ.get("API_ID"))
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMIN', '900873119').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMIN', '5253097982').split()]
