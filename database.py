2b23a84104b9410c3b7ba260fff9fb34eaae90f1
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-17 16:31:06 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,23 @@
+import pymongo 
+import os
+
+DB_NAME = os.environ.get("DB_NAME","Cloud19")
+mongo = pymongo.MongoClient(DB_URL)
+db = mongo[DB_NAME]
+dbcol = db["user"]
+
+
+def insert(chat_id):
+    user_id = int(chat_id)
+    user_det = {"_id": user_id,"file_id": None, "caption": None}
+    try:
+      dbcol.insert_one(user_det)
+    except:
+      pass
+
+def getid():
+    values = []
+    for key  in dbcol.find():
+         id = key["_id"]
+         values.append((id)) 
+    return values
