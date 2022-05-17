08d2c81b1fb59f9d130b94345799e062dbee1917
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-05-17 22:07:07 +0530

    Update Mute.py

--- ADV_GUARDIAN_GROOT/Mute.py
+++ ADV_GUARDIAN_GROOT/Mute.py
@@ -95,3 +95,35 @@ async def temp_mute_user(_, message):
                 " Mouth "
                 f" muted for {message.command[1]}!"
             )
+
+@Client.on_message(filters.command("unmute"))
+async def un_mute_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+
+    user_id, user_first_name = extract_user(message)
+
+    try:
+        await message.chat.unban_member(
+            user_id=user_id
+        )
+    except Exception as error:
+        await message.reply_text(
+            str(error)
+        )
+    else:
+        if str(user_id).lower().startswith("@"):
+            await message.reply_text(
+                "Okay, changed ... now "
+                f"{user_first_name} To "
+                " You can join the group!"
+            )
+        else:
+            await message.reply_text(
+                "Okay, changed ... now "
+                f"<a href='tg://user?id={user_id}'>"
+                f"{user_first_name}"
+                "</a> To "
+                " You can join the group!"
+            )
