50d6a43962f9319f2eb6bdd1dc1555c4358f9ff1
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-05-17 14:51:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/Zinan100/ADV-GUARDIAN-GROOT.git /ADV-GUARDIAN-GROOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /ADV-GUARDIAN-GROOT
 fi
-cd /Ajax
+cd /ADV-GUARDIAN-GROOT
 pip3 install -U -r requirements.txt
 echo "Starting GROOT....🔥"
 python3 bot.py
