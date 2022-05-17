f3a4bc0fe2b99b4188408b6f1c2c0ae24395e425
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-05-17 18:11:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ API_HASH = os.environ.get("API_HASH")
 API_ID = int(os.environ.get("API_ID"))
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMIN', '5253097982').split()]
+FORCE_SUB = os.environ.get("FORCE_SUB")
