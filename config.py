6dcbc1c544e600b5d3456ed0e9e1ab01977a9725
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-17 17:01:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
+import re
 import os
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
 
 API_HASH = os.environ.get("API_HASH")
 API_ID = int(os.environ.get("API_ID"))
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMIN', '900873119').split()]
