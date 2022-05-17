82989fe238ce7188d2bc14a14cc961ac630af917
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-05-17 14:12:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
 
 PyrogramBot = Client(
     "ADV GUARDIAN GROOT",
-    api_id="14057543", 
-    api_hash="f2c6f88a185a2019dbea0b19ad64b8f3",
-    bot_token="5226131888:AAGRJqN3cq4uDTkGh0TD43qGLfQiOuklhjQ",
+    api_id=API_ID, 
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
     plugins=dict(root="ADV_GUARDIAN_GROOT")
 )
 
