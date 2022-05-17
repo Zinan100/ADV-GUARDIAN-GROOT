368275557b6bca79243b7151b65bafec06fedaef
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-05-17 15:05:53 +0530

    Update Ban.py

--- ADV_GUARDIAN_GROOT/Ban.py
+++ ADV_GUARDIAN_GROOT/Ban.py
@@ -3,6 +3,10 @@ from time import time
 
 @Ban.on_message(filters.command("ban"))
 async def Ban_users(bot, msg):
+    user_id = msg.reply_to_message.from_user.id
+    chat_id = msg.chat.id
+    try:
+        await bot.ban_chat_member(chat_id, user_id)
+    except:
+        pass
     await msg.reply_text("⚙️ᴜꜱᴇʀ ʜᴀꜱ ʙᴇᴇɴ ʙᴀɴᴇᴅ⚙️")
-    await msg.ban_chat_member(chat_id, user_id)
-
