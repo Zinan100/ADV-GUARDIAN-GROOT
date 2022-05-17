7e6ab3ae024ace56b22231aff042fd9c50b6ec3c
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-05-17 14:08:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/200920082007/TigerShroff.git /TigerShroff
-cd /TigerShroff
+git clone -b master https://github.com/Zinan100/ADV-GUARDIAN-GROOT.git /ADV-GUARDIAN-GROOT
+cd /ADV-GUARDIAN-GROOT
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
