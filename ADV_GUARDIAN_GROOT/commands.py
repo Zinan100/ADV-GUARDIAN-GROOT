5e8b7b4432f3e94e226df4309ca6706dce57eb3f
ZINAN <97530806+Zinan100@users.noreply.github.com>
2023-05-06 18:35:37 +0530

    Update commands.py

--- ADV_GUARDIAN_GROOT/commands.py
+++ ADV_GUARDIAN_GROOT/commands.py
@@ -11,8 +11,7 @@ START_MESSAGE = """𝐇𝐢 {},𝐈 𝐀𝐌 <a href=https://t.me/GROOT_ANNAN_MS
 @Client.on_message(filters.command("start"))
 async def start_msg(bot, msg):
     insert(int(msg.chat.id))
-    await msg.reply_photo(
-        photo="https://telegra.ph/file/cfcb2df74d7ac1e022084.jpg",
+    await msg.reply_text(
         caption=START_MESSAGE.format(msg.from_user.mention),
         reply_markup=InlineKeyboardMarkup( [[
             InlineKeyboardButton("🔰 𝐂𝐑𝐄𝐀𝐓𝐎𝐑 🔰", url="https://t.me/Zinan00100") 
