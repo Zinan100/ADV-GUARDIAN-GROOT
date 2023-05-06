4309fc7dc341e398c36d3a43228dcc9720b82823
ZINAN <97530806+Zinan100@users.noreply.github.com>
2023-05-06 16:08:21 +0530

    Update commands.py

--- ADV_GUARDIAN_GROOT/commands.py
+++ ADV_GUARDIAN_GROOT/commands.py
@@ -1,7 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant
-from database import insert, getid
 from utils import not_subscribed
 from config import ADMIN
 
@@ -53,23 +52,3 @@ async def iid_msg(bot, msg):
            ]]
            )
     )
-
-@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
-async def broadcast(bot, message):
- if (message.reply_to_message):
-   ms = await message.reply_text("Geting All ids from database ...........")
-   ids = getid()
-   tot = len(ids)
-   await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
-   for id in ids:
-     try:
-     	await message.reply_to_message.copy(id)
-     except:
-     	pass
-
-@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["users"]))
-async def get_users(client: Client, message: Message):    
-    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
-    ids = getid()
-    tot = len(ids)
-    await msg.edit(f"Total uses = {tot}")
